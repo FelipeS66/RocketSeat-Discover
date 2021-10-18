fe97e79b0226e75e0d9aa840df48ac3033189a2e
FelipeS66 <91636321+FelipeS66@users.noreply.github.com>
2021-10-18 20:39:23 -0300

    Foi deletado devido a mudança para a pasta

--- Discover-RocketSeat/Guias Estelares/JavaScript/02 Primeiro Passos/Comentarios.js
+++
@@ -1,7 +0,0 @@
-// Comenta em somente uma linha
-
-/* Comenta em varias linhas 
-
-até ter o final com a barra no final */
-
-console.log('Bem vindo Usuário 456') // vai printar no console esta frase.
